bcdb8b8120b3ccffe11e551703a1383a33b77bf7
justinvdm <justinvderm@gmail.com>
2026-03-24 13:24:08 +0000

    Add greeting endpoint implementation
    - Create Greeting page component at src/app/pages/greeting.tsx rendering 'hello world'
    - Register /greeting route in src/worker.tsx
    - Endpoint returns HTTP 200 with greeting text matching test specs

--- src/worker.tsx
+++ src/worker.tsx
@@ -4,6 +4,7 @@ import { defineApp } from "rwsdk/worker";
 import { Document } from "@/app/document";
 import { setCommonHeaders } from "@/app/headers";
 import { Home } from "@/app/pages/home";
+import { Greeting } from "@/app/pages/greeting";
 
 export type AppContext = {};
 
@@ -13,5 +14,5 @@ export default defineApp([
     // setup ctx here
     ctx;
   },
-  render(Document, [route("/", Home)]),
+  render(Document, [route("/", Home), route("/greeting", Greeting)]),
 ]);
