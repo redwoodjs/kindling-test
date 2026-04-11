58c4171560586a4cda02c4e395f3e46da0028481
kindling <kindling@users.noreply.github.com>
2026-04-11 08:04:15 +0000

    add pure greeting() function returning 'hello world' [skip ci]

--- src/lib/greeting.ts
+++ src/lib/greeting.ts
@@ -1,5 +1,9 @@
+export function greeting(): string {
+  return "hello world"
+}
+
 export function greetingHandler(): Response {
-  return new Response("hello world", {
+  return new Response(greeting(), {
     headers: { "Content-Type": "text/plain" },
   })
 }
