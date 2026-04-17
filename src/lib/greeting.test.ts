2467d15736c742c52a2dfdefb1355619868c0407
justinvdm <justinvderm@gmail.com>
2026-04-17 13:53:05 +0000

    Add greeting utility returning hello world

---
+++ src/lib/greeting.test.ts
@@ -0,0 +1,10 @@
+import { describe, it } from "node:test"
+import assert from "node:assert"
+
+import { greeting } from "./greeting"
+
+describe("greeting", () => {
+  it("returns hello world", () => {
+    assert.strictEqual(greeting(), "hello world")
+  })
+})
