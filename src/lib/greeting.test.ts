11f96c195d75485b4f04265d7bba30a3827cf50b
justinvdm <justinvderm@gmail.com>
2026-04-19 20:25:11 +0000

    Tighten greeting behavior test

--- src/lib/greeting.test.ts
+++ src/lib/greeting.test.ts
@@ -6,8 +6,4 @@ describe("greeting", () => {
   it("returns hello world", () => {
     assert.strictEqual(greeting(), "hello world")
   })
-
-  it("returns the same greeting on repeated calls", () => {
-    assert.strictEqual(greeting(), greeting())
-  })
 })
