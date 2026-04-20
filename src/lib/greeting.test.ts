f0473d7e95e8fc5b52d9b53e83d785e2349188af
justinvdm <justinvderm@gmail.com>
2026-04-20 17:11:17 +0000

    Add greet() utility returning "hello world" with co-located test

---
+++ src/lib/greeting.test.ts
@@ -0,0 +1,7 @@
+import { test } from "node:test";
+import assert from "node:assert";
+import { greet } from "./greeting.ts";
+
+test("greet returns hello world", () => {
+  assert.strictEqual(greet(), "hello world");
+});
